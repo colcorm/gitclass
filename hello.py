27c24a3de24330e3db7e61648afc07a20448be63
Colin Cormier <colcorm@csa1.bu.edu>
2021-09-19 13:48:38 -0400

    Add function
    Subtract function

--- hello.py
+++ hello.py
@@ -1,4 +1,15 @@
 def hello_world():
     print('Hello World')
 
+def add_function():
+    x = int(input("Enter a value: "))
+    y = int(input("Enter another value: "))
+    sum = (x + y)
+    print(sum)
+
+def sub_function():
+    x = int(input("Enter a value: "))
+    y = int(input("Enter another value: "))
+    diff = (x + y)
+    print(diff)
 
